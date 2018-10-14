051d2f6ee0d0b1790473acb319dda380f14afa58
Paul <paullewa15@gmail.com>
2018-10-14 11:54:11 +0200

    Stdent

---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Geburtstag { get; set; }
+        private float Note { get; set; }
+
+        public Student(string Name, string Geburtstag, float Note)
+        {
+            this.Name = Name;
+            this.Geburtstag = Geburtstag;
+            this.Note = Note;
+
+
+        }
+    }
+}
