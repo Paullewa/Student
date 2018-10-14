c9d7d4296970ec531f31d6cf2fdd87d92fa0c8b1
DmitrySavkin <dimsaz@mail.ru>
2018-10-14 12:36:53 +0200

    Test correct

--- Program.cs
+++ Program.cs
@@ -10,15 +10,12 @@ namespace StudentSystem
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Student st = new Student("Paul", "16.04.1234.", 1.0);
-            Student tz = new Student(" Benir", "16.04.1234", 2.4);
-            Console.WriteLine(st.Studerenden(tz));
+            Student tz = new Student(" Benir", "16.04.1234.", 2.4);
+            Console.WriteLine(st.Studierenden(tz));
             Console.ReadKey();
-=======
-            
-            
->>>>>>> 3f518b45e6547a1c3a17e3f80df5800b04f1ca2c
+
         }
     }
 }
